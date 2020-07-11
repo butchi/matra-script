999130201e407cad261fd0ee7e6359e6e57f4767
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2020-07-11 16:59:08 +0900

    矩形描画実装

---
+++ js/script.js
@@ -0,0 +1,123 @@
+'use strict';
+
+const svgNs = 'http://www.w3.org/2000/svg';
+
+const svgCanvasElm = document.querySelector('.svg-canvas');
+
+const svgElm = document.createElementNS(svgNs, 'svg');
+svgElm.setAttribute('width', '610');
+svgElm.setAttribute('height', '377');
+svgElm.setAttribute('viewBox', '0 0 610 377');
+
+const g = document.createElementNS(svgNs, 'g');
+svgElm.appendChild(g);
+
+const vector = (...argArr) => {
+  if (argArr.length === 0) {
+    return vector(0, 0);
+  } else if (argArr.length === 1) {
+    const arg = argArr[0];
+
+    if (typeof arg === 'number') {
+      return vector(arg, 0);
+    } else if (arg instanceof Array) {
+      return vector(arg[0], arg[1]);
+    } else {
+      return arg;
+    }
+  } else if (argArr.length >= 2) {
+    return ({
+      x: argArr[0],
+      y: argArr[1],
+    });
+  }
+};
+
+const unit = 25;
+
+const stroke = {
+  color: null,
+  width: null,
+};
+
+let color = '#000';
+
+const convertSize = arg => {
+  if (typeof arg === 'undefined') {
+    return unit;
+  } else if (typeof arg === 'number') {
+    return arg * unit;
+  } else if (typeof arg === 'string') {
+    return arg;
+  } else {
+    return unit;
+  }
+}
+
+const setStroke = args => {
+  const color = args.color;
+  const width = args.width;
+
+  if (color) {
+    stroke.color = color;
+  }
+
+  if (width) {
+    stroke.width = width;
+  }
+}
+
+const setColor = arg => {
+  color = arg;
+}
+
+const rectangle = (...argArr) => {
+  let coord;
+  let size;
+
+  if (argArr.length === 0) {
+    coord = vector(0, 0);
+    size = vector(1, 1);
+  } else if (argArr.length === 1){
+    coord = vector(argArr[0].coord) || vector(0, 0);
+    size = vector(argArr[0].size) || vector(1, 1);
+  } else if (argArr.length === 2){
+    coord = vector(argArr[0]) || vector(0, 0);
+    size = vector(argArr[1]) || vector(1, 1);
+  } else {
+    coord = vector(0, 0);
+    size = vector(1, 1);
+  }
+
+  const elm = document.createElementNS(svgNs, 'rect');
+  elm.setAttribute('x', convertSize(coord.x));
+  elm.setAttribute('y', convertSize(coord.y));
+  elm.setAttribute('width', convertSize(size.x));
+  elm.setAttribute('height', convertSize(size.y));
+
+  if (stroke.color != null) {
+    elm.setAttribute('stroke', stroke.color);
+  }
+
+  if (stroke.width != null) {
+    elm.setAttribute('stroke-width', convertSize(stroke.width));
+  }
+
+  if (color != null) {
+    elm.setAttribute('fill', color);
+  }
+
+  return g.appendChild(elm);
+};
+
+[
+  setColor('red'),
+  setStroke({ width: 0.1, color: 'black' }),
+  rectangle({ coord: vector(8, 1), size: vector(3, 5) }),
+  setColor('green'),
+  rectangle([1, 3], [3, 3]),
+  setColor('blue'),
+  rectangle(),
+];
+
+svgCanvasElm.appendChild(svgElm);
