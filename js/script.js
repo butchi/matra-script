a8462a94aa1b6d1757f9492fa2e5af74f26f0d20
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2020-09-14 14:12:52 +0900

    SVG描画システム実装

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 'use strict';
 
 const canvas = [];
-const content = [{
-  canvas,
-}];
+const content = [];
+
+pdfMake.fonts = {
+  Roboto: {
+    normal: 'Roboto-Regular.ttf',
+  },
+}
 
 const docDefinition = {
 	content,
 	defaultStyle: {
-		color: 'gray',
+    color: 'gray',
 	}
 };
 
@@ -27,6 +31,8 @@ svgElm.setAttribute('viewBox', '0 0 610 377');
 
 const g = document.createElementNS(svgNs, 'g');
 svgElm.appendChild(g);
+
+let svgContentTxt = '';
 
 const vector = (...argArr) => {
   if (argArr.length === 0) {
@@ -49,7 +55,47 @@ const vector = (...argArr) => {
   }
 };
 
-const unit = 25;
+const angleVector = (...argArr) => {
+  let ret = vector(0, 0);
+
+  if (argArr.length === 0) {
+    ret = vector(1, 0);
+  } else if (argArr.length === 1) {
+    const arg = argArr[0];
+
+    if (typeof arg === 'number') {
+      ret = vector(Math.cos(arg), Math.sin(arg));
+    } else if (arg instanceof Array) {
+      ret = angleVector(arg[0], arg[1]);
+    } else {
+      ret = arg;
+    }
+  } else if (argArr.length === 2) {
+    const arg0 = argArr[0];
+    const arg1 = argArr[1];
+
+    let origin = vector(0, 0);
+    let offset = vector(1, 0);
+
+    if (typeof arg0 === 'number') {
+      const v = angleVector(arg1);
+      offset = vector(arg0 * v.x, arg0 * v.y);
+    } else {
+      origin = vector(arg0);
+      offset = angleVector(arg1);
+    }
+
+    ret = vector(origin.x + offset.x, origin.y + offset.y);
+  }
+
+  return ret;
+};
+
+let unit = 25;
+
+const font = {
+  size: 1,
+};
 
 const stroke = {
   color: null,
@@ -72,11 +118,39 @@ const convertSize = arg => {
   }
 }
 
-const setStroke = arg => {
-  const { color, width } = arg;
+const setUnit = arg => {
+  unit = arg;
 
-  stroke.color = color;
-  stroke.width = width;
+  return unit;
+}
+
+const setFont = arg => {
+  const { size, family } = arg;
+
+  font.size = size;
+  font.family = family;
+
+  return font;
+}
+
+const setStroke = arg => {
+  if (arg === undefined) {
+    stroke.color = 'black';
+    stroke.width = 1;
+  } else if (arg == null) {
+    stroke.color = null;
+    stroke.width = null;
+  } else {
+    const { color, width } = arg;
+
+    if (color != null) {
+      stroke.color = color;
+    }
+
+    if (width != null) {
+      stroke.width = width;
+    }
+  }
 
   return stroke;
 }
@@ -92,22 +166,26 @@ const setFace = arg => {
 const createSvgElement = ({ elementName, attribute, content }) => {
   let ret;
 
-  let svgElm;
-
-  svgElm = document.createElementNS(svgNs, elementName);
+  const element = document.createElementNS(svgNs, elementName);
 
   Object.keys(attribute).forEach(key => {
-    svgElm.setAttribute(key, attribute[key]);
+    element.setAttribute(key, attribute[key]);
   });
 
-  if (content) {
-    svgElm.innerHTML = content;
+  if (content != null) {
+    element.innerHTML = content;
   }
 
-  ret = svgElm;
+  const text = `<${elementName} ${Object.keys(attribute).map(key => {
+    return `${key}="${attribute[key]}"`;
+  }).join(' ')}>${content != null ? content : ''}</${elementName}>
+  `;
+ 
+  ret = { element, text };
 
   return ret;
 };
+
 
 const black = _ => setFace({
   color: 'black',
@@ -127,6 +205,47 @@ const blue = _ => setFace({
 const yellow = _ => setFace({
   color: 'yellow',
 });
+
+const line = (...argArr) => {
+  const propObj = {
+    coordArr: [vector(0, 0), vector(1, 0)],
+    stroke: Object.assign({}, stroke),
+  };
+
+  if (argArr.length === 0) {
+  } else if (argArr.length === 1){
+    propObj.coordArr = argArr[0].coordArray || [vector(0, 0), vector(1, 0)];
+  } else if (argArr.length === 2){
+    propObj.coordArr = argArr;
+  } else {
+  }
+
+  const attrObj = {
+    "x1": convertSize(propObj.coordArr[0].x),
+    "y1": convertSize(propObj.coordArr[0].y),
+    "x2": convertSize(propObj.coordArr[1].x),
+    "y2": convertSize(propObj.coordArr[1].y),
+  };
+
+  if (propObj.stroke.color != null) {
+    attrObj['stroke'] = propObj.stroke.color;
+  }
+
+  if (propObj.stroke.width != null) {
+    attrObj['stroke-width'] = propObj.stroke.width;
+  }
+
+  const { element: svgElm, text: svgTxt } = createSvgElement({
+    elementName: 'line',
+    attribute: attrObj,
+  });
+
+  svgContentTxt += svgTxt;
+
+  g.appendChild(svgElm);
+
+  return propObj;
+};
 
 const rectangle = (...argArr) => {
   const propObj = {
@@ -153,45 +272,26 @@ const rectangle = (...argArr) => {
     "height": convertSize(propObj.size.y),
   };
 
-  if (stroke.color != null) {
+  if (propObj.stroke.color != null) {
     attrObj['stroke'] = propObj.stroke.color;
   }
 
-  if (stroke.width != null) {
+  if (propObj.stroke.width != null) {
     attrObj['stroke-width'] = propObj.stroke.width;
   }
 
-  if (face.color != null) {
+  if (propObj.face.color != null) {
     attrObj['fill'] = propObj.face.color;
   }
 
-  const pdfObj = {
-    type: 'rect',
-    x: convertSize(propObj.coord.x),
-    y: convertSize(propObj.coord.y),
-    w: convertSize(propObj.size.x),
-    h: convertSize(propObj.size.y),
-  }
-
-  if (propObj.stroke.color != null) {
-    pdfObj.lineColor = propObj.stroke.color;
-  }
-
-  if (propObj.stroke.width != null) {
-    pdfObj.lineWidth = propObj.stroke.width;
-  }
-
-  if (propObj.face.color != null) {
-    pdfObj.color = propObj.face.color;
-  }
-
-  const svgElm = createSvgElement({
+  const { element: svgElm, text: svgTxt } = createSvgElement({
     elementName: 'rect',
     attribute: attrObj,
   });
 
+  svgContentTxt += svgTxt;
+
   g.appendChild(svgElm);
-  canvas.push(pdfObj);
 
   return propObj;
 };
@@ -208,6 +308,8 @@ const circle = (...argArr) => {
   } else if (argArr.length === 1){
     propObj.coord = vector(argArr[0].coord) || vector(0, 0);
     propObj.radius = argArr[0].radius || 1;
+    propObj.stroke = Object.assign({}, propObj.stroke, argArr[0].stroke);
+    propObj.face = Object.assign({}, propObj.face, argArr[0].face);
   } else if (argArr.length === 2){
     propObj.coord = vector(argArr[0]) || vector(0, 0);
     propObj.radius = argArr[1] || 1;
@@ -220,15 +322,15 @@ const circle = (...argArr) => {
     "r": convertSize(propObj.radius),
   };
 
-  if (stroke.color != null) {
+  if (propObj.stroke.color != null) {
     attrObj['stroke'] = propObj.stroke.color;
   }
 
-  if (stroke.width != null) {
+  if (propObj.stroke.width != null) {
     attrObj['stroke-width'] = propObj.stroke.width;
   }
 
-  if (face.color != null) {
+  if (propObj.face.color != null) {
     attrObj['fill'] = propObj.face.color;
   }
 
@@ -240,105 +342,88 @@ const circle = (...argArr) => {
     r2: convertSize(propObj.radius),
   };
 
-  if (stroke.color != null) {
-    pdfObj.lineColor = propObj.stroke.color;
-  }
-
-  if (stroke.width != null) {
-    pdfObj.lineWidth = propObj.stroke.width;
-  }
-
-  if (face.color != null) {
-    pdfObj.color = propObj.face.color;
-  }
-
-  const svgElm = createSvgElement({
+  const { element: svgElm, text: svgTxt } = createSvgElement({
     elementName: 'circle',
     attribute: attrObj,
   });
 
+  svgContentTxt += svgTxt;
+
   g.appendChild(svgElm);
-  canvas.push(pdfObj);
 
   return propObj;
 };
 
 const text = (...argArr) => {
   const propObj = {
-    text: '',
+    content: '',
     coord: vector(0, 0),
-    size: 1,
-    font: {},
+    font: Object.assign({}, font),
     stroke: Object.assign({}, stroke),
     face: Object.assign({}, face),
+    align: 0,
   };
 
   if (argArr.length === 0) {
   } else if (argArr.length === 1){
-    propObj.text = argArr[0].text || '';
-    propObj.coord = vector(argArr[0].coord) || vector(0, 0);
-    propObj.font.size = argArr[0].size || 1;
-  } else if (argArr.length === 2){
-    propObj.text = vector(argArr[0]) || '';
-    propObj.coord = vector(argArr[1]) || vector(0, 0);
-    propObj.font.size = 1;
-  } else if (argArr.length === 3){
-    propObj.text = vector(argArr[0]) || '';
-    propObj.coord = vector(argArr[1]) || vector(0, 0);
-    propObj.font.size = argArr[2];
+    if (content != null) {
+      propObj.content = argArr[0].content;
+    }
+    propObj.coord = vector(argArr[0].coord) || propObj.coord;
+    propObj.stroke = Object.assign({}, propObj.stroke, argArr[0].stroke);
+    propObj.face = Object.assign({}, propObj.face, argArr[0].face);
+    propObj.font = Object.assign({}, propObj.font, argArr[0].font);
+    propObj.align = argArr[0].align || propObj.align;
   } else {
   }
 
   const attrObj = {
     "x": convertSize(propObj.coord.x),
     "y": convertSize(propObj.coord.y),
-    "font-size": convertSize(propObj.font.size),
-  }
-
-  if (stroke.color != null) {
-    attrObj['stroke'] = propObj.stroke.color;
-  }
-
-  if (stroke.width != null) {
-    attrObj['stroke-width'] = propObj.stroke.width;
-  }
-
-  if (face.color != null) {
-    attrObj['fill'] = propObj.face.color;
-  }
-
-  const svgElm = createSvgElement({
-    elementName: 'text',
-    attribute: attrObj,
-    content: propObj.text,
-  });
-
-  const pdfObj = {
-    type: 'text',
-    color: propObj.face.color,
-    // pdfObj.font = `15pt Arial`;
-    // pdfObj.font = `${convertSize(size)}pt ${fontName}`;
-    text: propObj.text,
-    absolutePosition: {
-      x: convertSize(propObj.coord.x),
-      y: convertSize(propObj.coord.y),
-    },
   };
 
   if (propObj.stroke.color != null) {
-    pdfObj.lineColor = propObj.stroke.color;
+    attrObj['stroke'] = propObj.stroke.color;
   }
 
   if (propObj.stroke.width != null) {
-    pdfObj.lineWidth = propObj.stroke.width;
+    attrObj['stroke-width'] = propObj.stroke.width;
   }
 
   if (propObj.face.color != null) {
-    pdfObj.color = propObj.face.color;
+    attrObj['fill'] = propObj.face.color;
   }
 
+  if (propObj.font.family != null) {
+    attrObj['font-family'] = propObj.font.family;
+  }
+
+  if (propObj.font.size != null) {
+    attrObj['font-size'] = propObj.font.size;
+  }
+
+  if (propObj.align != null) {
+    attrObj['text-anchor'] = {
+      "-1": 'start',
+      "0": 'middle',
+      "1": 'end',
+    }[(propObj.align + 4) % 3 - 1];
+    attrObj['dominant-baseline'] = {
+      "-1": 'text-top',
+      "0": 'middle',
+      "1": 'baseline',
+    }[Math.floor((propObj.align + 1) / 3)];
+  }
+
+  const { element: svgElm, text: svgTxt } = createSvgElement({
+    elementName: 'text',
+    attribute: attrObj,
+    content: propObj.content,
+  });
+
+  svgContentTxt += svgTxt;
+
   g.appendChild(svgElm);
-  content.push(pdfObj);
 
   return propObj;
 };
@@ -350,27 +435,64 @@ $code.on('input', evt => {
   content.length = 0;
   content.push({ canvas });
   g.innerHTML = '';
+  svgContentTxt = '';
 
   try {
     eval(evt.target.value);
+    content.push({
+			svg: `<svg width="610" height="377" viewBox="0 0 610 377"><g>${
+        svgContentTxt
+      }</g></svg>`,
+			width: 600,
+			height: 400,
+    });
   } catch (e) {
     console.log(e);
   }
-})
+});
+
+
+const range = (...argArr) => {
+  let ret = [];
+  let start = 1;
+  let end = 1;
+
+  if (argArr.length === 0) {
+    start = 1;
+    end = 1;
+  } else if (argArr.length === 1) {
+    start = 1;
+    end = argArr[0];
+  } else {
+    start = argArr[0];
+    end = argArr[1];
+  }
+
+  for(let i = start; i <= end; i++) {
+    ret.push(i);
+  }
+
+  return ret;
+}
+
+const color = arg => {
+  return tinycolor(arg);
+};
 
 $code.val(`[
   red(),
-  setStroke({ width: 3, color: 'black' }),
-  rectangle({ coord: vector(8, 1), size: vector(3, 5) }),
+  setStroke({ width: 3, color: "black" }),
+  rectangle({ coord: vector(8, 3), size: vector(3, 5) }),
   green(),
-  rectangle([1, 3], [3, 3]),
+  rectangle([1, 5], [3, 3]),
   blue(),
   circle([6, 3], 1),
   setStroke({ width: 0 }),
   black(),
-  text('Thanks, world!', [1, 1], 1),
+  text({ text: "Thanks, world!", coord: vector(5, 1), size: 1, align: 0 }),
 ];`);
 
-$code.trigger('input');
-
 svgCanvasElm.appendChild(svgElm);
+
+$code.trigger('input');
+ 
